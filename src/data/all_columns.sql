427b999aec04ba3cba938e20e019286e3df00553
Brent <butlerbt.mg@gmail.com>
2019-12-03 11:07:37 -0800

    creats fuction out of sql query

--- src/data/all_columns.sql
+++ src/data/all_columns.sql
@@ -6,17 +6,26 @@
                 for our analysis of house sale price data
 */
 
-CREATE TABLE kc_data AS
-(SELECT sqfttotliving AS SqFt, 
-       s.saleprice AS SalePrice, 
-       b.address,
-       districtname, 
-       b.nbrlivingunits AS NumberOfUnits,
-       p.sqftlot,
-       (b.sqft1stfloor+b.sqftgarageattached) AS HouseFootprint,
-       ROUND((sqftgarageattached + sqft1stfloor)/sqftlot,4) AS ProportionHouse
+-- CREATE TABLE kc_data AS
+-- SELECT sqfttotliving AS SqFt, 
+--        s.saleprice AS SalePrice, 
+--        b.address,
+--        districtname, 
+--        b.nbrlivingunits AS NumberOfUnits,
+--        p.sqftlot,
+--        (b.sqft1stfloor+b.sqftgarageattached) AS HouseFootprint,
+--        ROUND((sqftgarageattached + sqft1stfloor)/sqftlot,4) AS ProportionHouse
+--        s.documentdate 
+-- FROM rpsale s
+-- INNER JOIN resbldg b ON CONCAT(s.Major,s.Minor) = CONCAT(b.Major, b.Minor)
+-- INNER JOIN parcel p ON CONCAT(s.Major,s.Minor) = CONCAT(p.Major,p.Minor)
+-- WHERE documentdate LIKE '%2018%'
+-- AND p.proptype = 'R');
+
+
+SELECT *
 FROM rpsale s
 INNER JOIN resbldg b ON CONCAT(s.Major,s.Minor) = CONCAT(b.Major, b.Minor)
 INNER JOIN parcel p ON CONCAT(s.Major,s.Minor) = CONCAT(p.Major,p.Minor)
 WHERE documentdate LIKE '%2018%'
-AND p.proptype = 'R');
+AND p.proptype = 'R';
